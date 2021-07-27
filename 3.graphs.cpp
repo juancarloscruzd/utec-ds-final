7059526cd466e38ab433e40f391c84862a915f0f
Juancarlos Cruz <juancarloscruzdiaz@gmail.com>
2021-07-27 12:02:42 -0500

    graphs

--- 3.graphs.cpp
+++ 3.graphs.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <utility>
 #include <queue>
+#include <set>
 
 using namespace std;
 //Clase Padre: Lista
@@ -193,6 +194,7 @@ public:
         this->arreglo[pos] = *elemento;
     }
 };
+//////////////////////////////////////////////
 
 //Clase Abstracta: Set
 template<typename Key> class Set{
@@ -296,6 +298,101 @@ public:
         return *(this->lista->eliminar());
     }
 };
+/////////////////////////////////////////
+
+//clase abstracta cola
+template<typename E> class Cola {
+private:
+    void operator = (const Cola&) {}  //Proteccion asignada
+    Cola(const Cola&) {}  //Constructor Proteccion copia
+public:
+    Cola(){}  //por defecto
+    virtual ~Cola() {}  //Base destructor
+
+    //Reinicializar la cola. El usuario es responsable
+    //por reclamar el espacio utilizado por el elemento
+    //de la cola
+    virtual void clear() = 0;
+
+    //colocar un elemento en la parte de atras de la cola
+    //it: el elemento siendo encolado
+    virtual void encolar( E) = 0;
+
+    //Remover y retornar elementos al frente de la cola
+    //Retornar: El elemento en el frente de la cola.
+    virtual E desencolar() = 0;
+
+    //Retornar: Una copia de el elemento frontal
+    virtual E valorFrontal() = 0;
+
+    //Retornar: El numero de elementos in la cola .
+    virtual int longitud() = 0;
+};
+
+template <typename E> class ACola : public Cola<E> {
+private:
+    int tamMax;  //Tamaño maximo de la cola
+    int frente;     //Indice del elemento frontal
+    int elemFin;    //Indice del elemento final
+    E *listaArreglo;  //Arreglo conteniendo los elementos de la cola
+
+public:
+
+    /*
+     * Constructor
+     */
+    ACola(int l=100){
+        this->tamMax = l;
+        this->frente=1;
+        this->elemFin=0;
+        this->listaArreglo = new E[this->tamMax+1];
+    }
+
+    /*
+     * Destructor
+     */
+    ~ACola(){
+        //limpiar();
+    }
+
+    void encolar(E val){
+        //Al ENCOLAR, el puntero "elemFin" se calcula como "elemFin = (elemFin+1) % TamMax"
+        //TamMax = TamUsuario + 1
+        //this->listaArreglo[(elemFin+1)%(tamañoMax+1)] = val;
+        this->listaArreglo[elemFin+1] = val;
+        this->elemFin++;
+    }
+
+    E desencolar(){
+        //Al DESENCOLAR, el puntero "frente" se calcula como "frente = (frente + longitud()) % TamMax"
+        //TamMax = TamUsuario + 1
+        E val = this->listaArreglo[frente];
+        //this->frente = (this->frente + longitud()) % (tamañoMax+1);
+        this->frente++;
+        return val;
+    }
+
+    E valorFrontal(){
+        return this->listaArreglo[frente];
+    }
+
+    int longitud(){
+        if(this->elemFin==this->tamMax){
+            return tamMax;
+        }else {
+            return (this->tamMax - this->frente + this->elemFin + 1) % (this->tamMax);
+        }
+    }
+
+    void clear(){
+        this->frente=1;
+        this->elemFin=0;
+        //this->listaArreglo[this->frente]=NULL;
+    }
+
+
+};
+/////////////////////////////////////////
 
 
 class Grafo{
@@ -337,6 +434,7 @@ private:
     int numeroVertices;
     int numeroAristas;
     int **matriz;
+    int **matrizw;
     int *marca;
 
     ListaArreglo<bool> *visitado = new ListaArreglo<bool>;
@@ -360,12 +458,24 @@ public:
         for (i = 0; i < this->numeroVertices; i++) {
             this->matriz[i] = new int[this->numeroVertices];
         }
-        //Debemos colocar a la matriz con valores CERO
+        //Debemos colocar a la matriz adyacencia con valores CERO
         for (i = 0; i < this->numeroVertices; i++) {
             for (j = 0; j < this->numeroVertices; j++) {
                 this->matriz[i][j] = 0; //Peso CERO
             }
         }
+        //Inicializar la matriz de pesos(w)
+        this->matrizw = new int*[this->numeroVertices];
+        for (i = 0; i < this->numeroVertices; i++) {
+            this->matrizw[i] = new int[this->numeroVertices];
+        }
+        //Completar con un valor la matriz de pesos(w).Valor muy negativo.
+        for (i = 0; i < this->numeroVertices; i++) {
+            for (j = 0; j < this->numeroVertices; j++) {
+                this->matrizw[i][j] = -1000; //Peso CERO
+            }
+        }
+        //Colocando en false la matriz visitado
         for (i = 0; i < this->numeroVertices; i++) {
             this->visitado->setValorEnPosicion(i,false);
         }
@@ -442,12 +552,14 @@ public:
     // i, j: los vertices
     // w: peso
     void setArista(int i, int j, int w) {
-        this->matriz[i][j] = w;
+        this->matriz[i][j] = 1;
+        this->matrizw[i][j] = w;
     }
 
     //Eliminar arista
     void eliminarArista(int i, int j) {
         this->matriz[i][j] = 0;
+        this->matrizw[i][j] = -1000;
     }
 
     //Determinar si una arista esta en el grafo
@@ -459,7 +571,7 @@ public:
 
     //Retornar el peso de una arista
     int getPeso(int i, int j) {
-        return this->matriz[i][j];
+        return this->matrizw[i][j];
     }
 
     //Valores de marca para cada nodo o vertice
@@ -473,6 +585,7 @@ public:
         this->marca[v] = val;
     }
 
+    //La funcion adyacencia retorna una lista con el contenido de todas las aristas con el resto de Vertices.
     void funcion_adyacencia(int v,ListaArreglo<int> **vector){
         ListaArreglo<int> *result = new ListaArreglo<int>;
         //cout<<vector;
@@ -485,6 +598,7 @@ public:
     //Recorrer el grafo con el metodo Depth-first search
     void RecorridoDFS(int v){
         cout<<v<<" ";
+        //Previsit(G,v); //util para realizar alguna tarea antes de marcar como visitado al vertice.
         this->visitado->setValorEnPosicion(v,true);
         for (int i = 0; i < this->numeroVertices; ++i) {
             if(matriz[v][i]!=0){
@@ -493,41 +607,44 @@ public:
                 }
             }
         }
+        //PostVisit(G,v); //util para realizar alguna tarea luego de la ejecucion del traverse.
     }
+
     //Recorrer el Grafo con el metodo Breadth-first search
     void RecorridoBFS(int v){
+        // Esta lista se puede entender tambien como si fue Visitado el vértice.
         ListaArreglo<int> *distancia = new ListaArreglo<int>;
         for (int i = 0; i < this->numeroVertices; i++) {
             distancia->setValorEnPosicion(i,-1);
         }
-        queue<int> q;
-        q.push(v);
+        ACola<int> *q1 = new ACola<int>();
+        q1->encolar(v);
         distancia->setValorEnPosicion(v,0);
 
-        while(!q.empty()){
-            int u = q.front();
+        while(q1->longitud() != 0){
+            int u = q1->desencolar();
             cout<<u<<" "; // Mostrar los vertices
-            q.pop();
             for (int i = 0; i < this->numeroVertices; ++i) {
                 if(matriz[u][i]!=0){
-                    if(distancia->getValorEnPosicion(i)== -1){
+                    if(distancia->getValorEnPosicion(i)== -1){ //El -1 puede actuar como un check de No-Visitado.
                         distancia->setValorEnPosicion(i,distancia->getValorEnPosicion(u) + 1);
-                        q.push(i);
+                        q1->encolar(i);
                     }
                 }
             }
         }
         cout << endl;
         //Para mostrar la distancia en aristas hasta todos los otros vertices.
-
+        /*
         for (int i = 0; i < this->numeroVertices; i++) {
             cout << distancia->getValorEnPosicion(i) << ' ';
         }
-
+        */
         cout << endl;
         delete distancia;
     }
 
+    //Devuelve el numero de saltos (aristas) desde el vertice v1 hasta el vertice v2.
     int NumSaltos(int v1, int v2){
         ListaArreglo<int> *distancia = new ListaArreglo<int>;
         for (int i = 0; i < this->numeroVertices; i++) {
@@ -552,50 +669,113 @@ public:
         return distancia->getValorEnPosicion(v2);
     }
 
+    int Prim(){
+        //Colocando en false la matriz visitado
+        for (int i = 0; i < this->numeroVertices; i++) {
+            this->visitado->setValorEnPosicion(i,false);
+        }
+        set<pair<int, int>> pq;
+        pq.insert({0, 0}); //Primer costo y primer Vertice.
+
+        int mst = 0;
+        while (!pq.empty()) {
+            auto [w, v] = *pq.begin();
+            pq.erase(pq.begin());
+            if (this->visitado->getValorEnPosicion(v))
+                continue;
+            mst += w;
+            cout<<v<<" ";
+            this->visitado->setValorEnPosicion(v,true);
+            for (int i = 0; i < this->numeroVertices; ++i) {
+                if(matriz[v][i]!=0){
+                    int u = i;
+                    int w = this->matrizw[v][i];
+                    pq.insert({w,u});
+                }
+            }
+        }
+        cout<<endl;
+        return mst;
+    }
+
+    void Dijkstra(int s, ListaArreglo<int> **x){
+        set<pair<int, int>> pq;
+
+        //Arreglo para distancias mínimas de un Vertice hacia el resto.
+        ListaArreglo<int> *distancia = new ListaArreglo<int>(this->numeroVertices);
+        for (int i = 0; i < this->numeroVertices; i++) {
+            distancia->agregar(1000000);
+        }
+        //Arreglo de padres para calcular el camino.
+        ListaArreglo<int> *path = new ListaArreglo<int>(this->numeroVertices);
+        for (int i = 0; i < this->numeroVertices; i++) {
+            path->agregar(-1);
+        }
+
+        distancia->setValorEnPosicion(s,0);
+        pq.insert({0, s});
+
+        while (!pq.empty()) {
+            auto[d, v] = *pq.begin();
+            pq.erase(pq.begin());
+
+            if (d != distancia->getValorEnPosicion(v))
+                continue;
+
+            for (int i = 0; i < this->numeroVertices; ++i) {
+                if(matriz[v][i]!=0){
+                    int u = i;
+                    int w = this->matrizw[v][i];
+                    //cout<<u<<" - "<<w<<endl;
+                    if(distancia->getValorEnPosicion(v) + w < distancia->getValorEnPosicion(u)){
+                        path->setValorEnPosicion(u,v);
+                        distancia->setValorEnPosicion(u,distancia->getValorEnPosicion(v) + w);
+                        pq.insert({distancia->getValorEnPosicion(u), u});
+                    }
+                }
+            }
+        }
+        *x = distancia;
+    }
 };
 
 int main() {
 
     GrafoMA *grafo = new GrafoMA(6);
     ListaArreglo<int> *fadyacencia = new ListaArreglo<int>;
+    ListaArreglo<int> *t = new ListaArreglo<int>;
 
-    grafo->setArista(0,1, 1);
-    grafo->setArista(0,3, 1);
-    grafo->setArista(1,0, 1);
-    grafo->setArista(1,4, 1);
-    grafo->setArista(1,2, 1);
-    grafo->setArista(1,3, 1);
-    grafo->setArista(2,1, 1);
-    grafo->setArista(2,3, 1);
-    grafo->setArista(3,0, 1);
-    grafo->setArista(3,1, 1);
-    grafo->setArista(3,2, 1);
-    grafo->setArista(3,5, 1);
-    grafo->setArista(4,1, 1);
-    grafo->setArista(5,3, 1);
+    grafo->setArista(0,1, 11);
+    grafo->setArista(0,3, 12);
+    grafo->setArista(1,0, 11);
+    grafo->setArista(1,4, 12);
+    grafo->setArista(1,2, 10);
+    grafo->setArista(1,3, 6);
+    grafo->setArista(2,1, 10);
+    grafo->setArista(2,3, 14);
+    grafo->setArista(3,0, 12);
+    grafo->setArista(3,1, 6);
+    grafo->setArista(3,2, 14);
+    grafo->setArista(3,5, 7);
+    grafo->setArista(4,1, 12);
+    grafo->setArista(5,3, 7);
 
-    grafo->RecorridoDFS(0);
-    cout<<endl;
-    grafo->RecorridoBFS(3);
-    cout<<grafo->NumSaltos(3,4)<<endl;
+    //grafo->RecorridoDFS(1);
+    //cout<<endl;
+    //grafo->RecorridoBFS(1);
+    //cout<<grafo->Prim()<<endl;
 
-    grafo->funcion_adyacencia(3,&fadyacencia);
-
-    for(int j=0;j<fadyacencia->longitud();j++) {
-        cout << fadyacencia->getValorEnPosicion(j) << " ";
+    grafo->Dijkstra(5,&t);
+    for(int j=0;j<t->longitud();j++) {
+        cout << t->getValorEnPosicion(j) << " ";
     }
+    
+    //grafo->funcion_adyacencia(3,&fadyacencia);
 
+    //for(int j=0;j<fadyacencia->longitud();j++) {
+    //    cout << fadyacencia->getValorEnPosicion(j) << " ";
+    //}
 
-/*
-    int peru = 1;
-    int vecino = grafo->primero(peru);
-    cout<<"Vecino: "<<vecino<<endl;
-    while(vecino<grafo->get_N_Vertices()){
-        vecino = grafo->siguiente(peru, vecino);
-        if(vecino < grafo->get_N_Vertices())
-            cout<<"Vecino: "<<vecino<<endl;
-    }
-*/
     delete grafo;
 
     return 0;
